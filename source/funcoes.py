eae2e5eb52f5e245f0b6a94767d2ed36d9e93986
marcelo <marques.marcelo.rodrigues@gmail.com>
2025-05-25 17:09:45 -0300

    feat: Adaptar Criptografar e Decifrar Para ',. ' e Especiais

--- source/funcoes.py
+++ source/funcoes.py
@@ -1,48 +1,34 @@
-ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+from unidecode import unidecode
 
-frequencia_portugues = {
-    "A": 0.1463,
-    "B": 0.0104,
-    "C": 0.0388,
-    "D": 0.0499,
-    "E": 0.1257,
-    "F": 0.0102,
-    "G": 0.0130,
-    "H": 0.0078,
-    "I": 0.0618,
-    "J": 0.0040,
-    "K": 0.0002,
-    "L": 0.0278,
-    "M": 0.0474,
-    "N": 0.0505,
-    "O": 0.1073,
-    "P": 0.0252,
-    "Q": 0.0120,
-    "R": 0.0653,
-    "S": 0.0680,
-    "T": 0.0434,
-    "U": 0.0368,
-    "V": 0.0158,
-    "W": 0.0004,
-    "X": 0.0021,
-    "Y": 0.0001,
-    "Z": 0.0047,
-}
-# normaliza de acordo com alfabeto
+ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+CARACTERES_PERMITIDOS = " .,"
 
 def normalizar_txt(texto):
+    texto = unidecode(texto)
     txt_processado = ""
     for char in texto.upper():
         if char in ALFABETO:
             txt_processado += char
+        if char in CARACTERES_PERMITIDOS:
+            txt_processado += char
+
     return txt_processado
 
 
-def extender_chave(chave, tamanho_texto): #extende e normaliza a chave
+def extender_chave(chave, tamanho_texto):  # extende e normaliza a chave
     chave_normalizada = ""
     for char in chave.upper():
         if char in ALFABETO:
             chave_normalizada += char
     if not chave_normalizada:
-        return "Chave Inválida"    
-    return (chave_normalizada * (tamanho_texto // len(chave_normalizada) + 1))[:tamanho_texto]
+        return "Chave Inválida"
+    return (chave_normalizada * (tamanho_texto // len(chave_normalizada) + 1))[
+        :tamanho_texto
+    ]
+
+def obter_apenas_letras(texto):
+    apenas_letras = ""
+    for char in texto: 
+        if char in ALFABETO:
+            apenas_letras += char
+    return apenas_letras
--- source/main.py
+++ source/main.py
@@ -2,39 +2,64 @@ from funcoes import *
 import argparse
 
 
-def criptografar( chave , texto ): #  chave e plaintext para o Vigenere
+def criptografar(chave, texto):  #  chave e plaintext para o Vigenere
 
-    chave_normalizada = extender_chave( chave , len(texto))
-    plaintext_normalizado = normalizar_txt( texto )
+    chave_normalizada = extender_chave(chave, len(texto))
+    plaintext_normalizado = normalizar_txt(texto)
     texto_cifrado = []
+    indice = 0
 
-    for i in range(len(plaintext_normalizado)):
-        
-        itexto = plaintext_normalizado[i]
-        ichave = chave_normalizada[i]
+    for char_do_texto in plaintext_normalizado:
+        if char_do_texto in ALFABETO:
 
-        valor_char_texto = ALFABETO.find(itexto)
-        valor_char_chave = ALFABETO.find(ichave)
+            valor_char_texto = ALFABETO.find(char_do_texto)
+            valor_char_chave = ALFABETO.find(chave_normalizada[indice])
 
-        valor_cifrado = (valor_char_texto + valor_char_chave) % 26
-        texto_cifrado.append(ALFABETO[valor_cifrado])
-    return ''.join(texto_cifrado)
+            valor_char_cifra = (valor_char_texto + valor_char_chave) % 26
+            texto_cifrado.append(ALFABETO[valor_char_cifra])
+            indice += 1
+        else:
+            texto_cifrado.append(char_do_texto)
+            
+            
+            
+    return "".join(texto_cifrado)
 
-def decifrar( chave , texto_cifrado ):
+
+def decifrar(chave, texto_cifrado):
     texto_original = []
-    chave_normalizada = extender_chave( chave , len(texto_cifrado))
+    indice = 0
+    
+    chave_normalizada = extender_chave(chave, len(texto_cifrado))
+    texto_cifrado = normalizar_txt(texto_cifrado)
 
-    for i in range(len(texto_cifrado)):
+    for char_da_cifra in texto_cifrado:
+        if char_da_cifra in ALFABETO:
+            valor_char_cifra = ALFABETO.find(char_da_cifra)
+            valor_char_chave = ALFABETO.find(chave_normalizada[indice])
 
-        icifrado = texto_cifrado[i]
-        ichave = chave_normalizada[i]
+            valor_char_texto = (valor_char_cifra - valor_char_chave + 26) % 26
+            texto_original.append(ALFABETO[valor_char_texto])
+            indice += 1
+        else:
+            texto_original.append(char_da_cifra)
+    return "".join(texto_original)
+            
+print(criptografar(input("  chave: "), input("  texto: ")))
 
-        valor_char_cifrado = ALFABETO.find(icifrado)
-        valor_char_chave = ALFABETO.find(ichave)
+print(decifrar(input("  chave: "), input("  texto: ")))
 
-        valor_char_original = (valor_char_cifrado - valor_char_chave + 26) % 26
-        texto_original.append(ALFABETO[valor_char_original])
-    return ''.join(texto_original)
-
-print(criptografar("marcelo", "teste 1 do trabalho, simples para verificar o algoritmo"))
-print(decifrar("marcelo" , criptografar("marcelo", "teste 1 do trabalho, simples para verificar o algoritmo") ))
+    
+"""
+print(
+    criptografar("marcelo", "teste 1 do trabalho, simples para verificar o algoritmo")
+)
+print(
+    decifrar(
+        "marcelo",
+        criptografar(
+            "marcelo", "teste 1 do trabalho, simples para verificar o algoritmo"
+        ),
+    )
+)
+"""
